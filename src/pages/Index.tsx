bb925f0c096e4ce2c23bd2aec51fc8b09ed2f1ce
ahmedhussainbca <ahmedhussainbca@gmail.com>
2025-11-08 06:56:35 +0000

    update scroll to up issue

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,15 @@
 
 import React from 'react';
+import { useEffect } from 'react';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import CompanyIntro from '../components/CompanyIntro';
 import Footer from '../components/Footer';
 
 const Index = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scrolls to the very top (x=0, y=0)
+  }, []);
   return (
     <div className="min-h-screen bg-white">
       <Header />
